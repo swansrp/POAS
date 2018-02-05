3e8a4f395acbf33cb8d02687c62eb07c80c8bef5
swansrp <56093273@qq.com>
2018-02-05 15:52:37 +0800

    add jsp

--- src/main/java/com/srct/ril/poas/springboot/HelloController.java
+++ src/main/java/com/srct/ril/poas/springboot/HelloController.java
@@ -1,5 +1,9 @@
 package com.srct.ril.poas.springboot;
 
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
  
@@ -9,6 +13,12 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello Spring Boot!";
+    }
+    
+    @RequestMapping("/hellojsp")
+    public String hello(Model m) {
+    	m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "Hello";
     }
  
 }
